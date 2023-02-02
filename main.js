a8092c5a4a20bec5e74f8dc76617f817e512fb4c
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-02 12:26:35 +0300

    усложненное присвоение значений

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
 'use strict'
 
 const num = 266219;
-const num2 = String(num);
-let resul = 1;
-let degree = 1;
-let resul2 = 1;
+let num2 = String(num.toString().split('').reduce((elem, item) => elem * item));
+console.log(num2);
 
-for (let i = 0; i < num2.length; i++) {
-    resul *= num2[i];  
-};
-
-console.log(resul);
-
-function foo() {
-    resul2 *= resul;
-    if (degree === 3) return;
-    degree++;
-    foo();
-};
-
-foo();
+num2 = String((num2)**3);
+console.log(num2.slice(0,2));
