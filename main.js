71c42f25113eb1501564f41b18575bdb08d728ac
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-03 14:16:44 +0300

    взаимодействие с пользователем усложненное

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 'use strict'
 
-const num = 266219;
-let num2 = String(num.toString().split('').reduce((elem, item) => elem * item));
-console.log(num2);
+const lang = document.documentElement.lang;
+let counter;
+let nameG = 'Анзор';
 
-num2 = String((num2)**3);
-console.log(num2.slice(0,2));
+if (lang == 'ru') {
+    counter = 0;
+} else {
+    counter = 1;
+};
+
+let arr = [
+    ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+];
+
+function foo(i) {
+    console.log(String(arr[i]));
+};
+
+foo(counter);
+
+nameG == 'Артем' ? console.log('директор')  : nameG == 'Александр' ? console.log('преподавватель') : console.log('студент');
