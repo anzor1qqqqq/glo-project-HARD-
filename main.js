6f82242eaa9ac651c3f0b974800941e1492816c8
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-07 17:10:29 +0300

    День недели

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 'use strict'
 
-const arr = ['123', '234', '456', '6565', '45454', '676', '978'];
+const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const weekToday = new Date().getDay();
 
-arr.forEach(item => {
-    if (item[0] === '2' || item[0] === '4') console.log(item);
-});
-
-for (let i = 1; i <= 100; i++) {
-    if (i === 1) continue;
-
-    if (i === 2 || i === 3 || i === 5) {
-        console.log(i);
-        console.log('Делители этого числа: 1 и ' + i);
-    } else if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
-        console.log(i);
-        console.log('Делители этого числа: 1 и ' + i);
+for (let i = 0; i < week.length; i++) {
+    if (i === 0 || i === 6) {
+        console.log(`%c${week[i]}\n`, 'font-style: italic;');
+        continue;
     };
+
+    if (i === weekToday) {
+        console.log(`%c${week[i]}\n`, 'font-weight: bold;');
+        continue; 
+    };
+
+    console.log(`${week[i]}\n`);
 };
