c5cade88957c99f652bbcd5da3fe11c2306f8a01
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-08 18:53:55 +0300

    modified:   index.html 	modified:   main.js 	new file:   style.css

--- main.js
+++ main.js
@@ -1,131 +1,36 @@
 'use strict'
 
-const reg = /^\d+$/;
+const lessonOne = document.querySelector('.lesson_one');
+const lessonTwo = document.querySelector('.lesson_two');
 
-const appData = {
-    start: function() {
-        this.asking();
-        this.logger();
-    },
+const date = new Date();
+const todayDay = date.getDate();
+const todayYear = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+let hourText;
 
-    title: '',
-    screens: [],
-    adaptive: true,
-    services: {},
-    fullPrice: 0,
+const arrMonth = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+const arrWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-    asking: function() {
-        let screenPrice;
-        let servicePrice;
+const todayMonth = arrMonth.find((item, index) => {
+    return date.getMonth() === index;
+});
 
-        this.title = prompt('Как называется ваш проект?');
+const todayWeek = arrWeek.find((item, index) => {
+    return date.getDay() === index;
+});
 
-        while (!isNaN(this.title.trim())) {
-            this.title = prompt('Как называется ваш проект?');  
-        }; 
+console.log(hours);
 
-        for (let i = 0; i < 2; i++) {
-            let screenInfo = prompt('Какие типы экранов нужно разработать?');
+if (hours === 1 || hours === 13) {
+    hourText = 'час';
+} else if (hours >= 2 && hours <= 4)  {
+    hourText = 'часа';
+} else {
+    hourText = 'часов';
+};
 
-            while (!isNaN(screenInfo.trim())) {
-                screenInfo = prompt('Какие типы экранов нужно разработать?');
-            };
-
-            screenPrice = prompt('Сколько будет стоить данная работа?');
-
-            while (!this.fooProgram.protectNum(screenPrice)) {
-                screenPrice = prompt('Сколько будет стоить данная работа?');
-            };
-
-            this.screens.push({name: screenInfo, price: screenPrice});
-        };
-
-        this.adaptive = confirm('Нужен ли адаптив на сайте?');
-
-        do {
-            screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
-        } while (!this.fooProgram.protectNum(screenPrice));
-        
-        for (let i = 0; i < 2; i++) {
-            let service = prompt('Какой дополнительный тип услуги нужен?');
-
-            while (!isNaN(service.trim())) {
-                service = prompt('Какой дополнительный тип услуги нужен?');
-            };
-
-            servicePrice = prompt('Сколько это будет стоить?');
-
-            while (!this.fooProgram.protectNum(servicePrice)) {
-                servicePrice = prompt('Сколько это будет стоить?');
-            };   
-
-            this.services[service[i] || 'key'] = servicePrice;
-        };
-
-        screenPrice = this.screens.reduce((elem, item) => {
-            return elem + +item.price;
-        }, 0);
-
-
-        for (let key in this.services) {
-            servicePrice += +this.services[key];
-        };
-
-        this.fullPrice = this.fooProgram.getFullPrice(screenPrice, servicePrice);
-
-        switch (true) {
-            case this.fullPrice > 30000:
-                this.fullPrice -= this.fullPrice * 0.1;
-                alert('Сумма заказа составила больше 30000 рублей, в связи с этим делаем вам скидку 10%');
-                break;
-            case this.fullPrice > 15000 && this.fullPrice < 30000:
-                this.fullPrice -= this.fullPrice * 0.05;
-                alert('Сумма заказа составила больше 15000 рублей, в связи с этим делаем вам скидку 5%');
-                break;
-            case this.fullPrice < 15000 && this.fullPrice > 0: 
-                alert('Скидка не предусмотрена');
-                break;
-            case this.fullPrice <= 1000:
-                alert('Что-то пошло не так');
-                break;
-            default:
-                break;
-        };
-
-        this.fooProgram.getTitle();
-        this.fooProgram.servicePercentPrice(); 
-    },
-
-    fooProgram: {
-        protectNum: function(num) {
-            return reg.test(num);
-        },
-
-        allServicePrices: function getAllServicePrices(a, b) {
-            return Number(a + b);
-        },
-
-        getFullPrice: function(a, b) {
-            return a + b;
-        },
-
-        getTitle: function() {
-            if (appData.title)  {  
-                appData.title = appData.title.trim();
-                appData.title = appData.title[0].toUpperCase() + appData.title.slice(1).toLowerCase() ;
-            };
-        },
-
-        servicePercentPrice: function getServicePercentPrices() {
-            appData.fullPrice = Math.ceil(appData.fullPrice - (appData.fullPrice * 0.1));
-        },
-    },
-
-    logger: function () {
-        for (const key in appData) {
-            console.log(appData[key]);
-        };
-    },
-};   
-
-appData.start();
+lessonOne.textContent = 'Сегодня ' + todayWeek + ', ' + todayDay + ' ' + todayMonth + ' ' + todayYear + ' года, ' + hours + ' ' + hourText + ' ' + minutes + ' минут ' + seconds + ' секунды' ;
+lessonTwo.textContent = date.toLocaleString().replace(',', ' -');
